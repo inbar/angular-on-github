2fdf24e7c7f559d3fb599fa8ca73caa347378ae8
inbar <poupik@gmail.com>
2016-03-15 12:56:33 +0100

    some data binding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,10 @@
  * Controller of the angularOnGithubApp
  */
 angular.module('angularOnGithubApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.items = [
+      'item 1',
+      'item 2',
+      'item 3'
     ];
   });
