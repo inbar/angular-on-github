4df188ca1a55a6eb1f0060e0ef4df0a97bf82650
inbar <poupik@gmail.com>
2016-03-15 08:49:55 +0100

    init angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name developmentApp
+ * @description
+ * # developmentApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('developmentApp', []);
